3355140a33234e8703c584e9d9e7fc454787d01f
DESKTOP-BD4B31F\user <wlsflcnrn123@gmail.com>
2023-09-26 20:13:01 +0900

    [김동휘]  - aws배포 설정

--- src/main/java/com/ohana0/ohanaMarket/OhanaMarketApplication.java
+++ src/main/java/com/ohana0/ohanaMarket/OhanaMarketApplication.java
@@ -2,9 +2,10 @@ package com.ohana0.ohanaMarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OhanaMarketApplication {
+public class OhanaMarketApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(OhanaMarketApplication.class, args);
